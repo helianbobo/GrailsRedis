6c581ad14eb63a6a9b53e5381f975df9d7c36599
liuchao <liuchao36@gmail.com>
2012-05-03 09:47:40 +0800

    init

--- grails-app/controllers/grailsredis/RedisController.groovy
+++ grails-app/controllers/grailsredis/RedisController.groovy
@@ -17,19 +17,19 @@ class RedisController {
         def from = params.from_date
         def to = params.to_date
 
-        def count = Ticket.countByDatetimeCreatedBetween(from, to)
+        def hql = """
+        select id, datetimePosted, clientAccountId, subjectId, normalisedSentimentScore
+        from Ticket
+        where datetimePosted >= ? and
+        datetimePosted <= ?
+        """
+
+        def count = Ticket.countByDatetimePostedBetween(from, to)
         def current = 0
         while(current < count){
-            def query = Ticket.where {
-                datetimeCreated <= to && datetimeCreated >= from
-            }
-            def list = query.list(max: 10000, offset: current)
+            def list = Ticket.executeQuery(hql, [from, to], [max: 100000, offset: current])
 
-            StopWatch stopWatch = new StopWatch("pushTpRedis")
-            stopWatch.start()
             pushToRedis(list)
-            stopWatch.stop()
-            println stopWatch.prettyPrint()
 
             current += list.size()
 
@@ -45,9 +45,15 @@ class RedisController {
         def queryName = "overview"
 
         redisService.withPipeline {Pipeline pipeline ->
-            list.each{Ticket ticket->
-                def key = "${queryName}:${ticket.clientAccountId}:${ticket.subjectId}:${ticket.datetimeCreated.format("yyMMdd")}:${ticket.sentiment}"
-                pipeline.sadd(key, "${ticket.id}")
+            list.each{ticket->
+                def id = ticket[0]
+                def datetimePosted = ticket[1]
+                def clientAccountId = ticket[2]
+                def subjectId = ticket[3]
+                def sentiment = new Ticket(normalisedSentimentScore:ticket[4]).sentiment
+
+                def key = "${queryName}:${clientAccountId}:${subjectId}:${datetimePosted.format("yyMMdd")}:${sentiment}"
+                pipeline.sadd(key, "${id}")
             }
         }
     }
